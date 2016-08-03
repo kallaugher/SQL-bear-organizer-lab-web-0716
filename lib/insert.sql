7b1aa9b17e4f6d71499be2c4fd007bbc0c5aabea
kallaugher <alicekallaugher@gmail.com>
2016-08-03 17:27:03 -0400

    finish bear queries

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'smooth', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 7, 'M', 'white', 'rowdy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 9, 'F', 'black', 'kind', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 6, 'F', 'black', 'gruff', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 11, 'F', 'brown', 'excitable', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 9, 'M', 'green', 'grumpy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 4, 'F', 'brown', 'lazy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 3, 'M', 'brown', 'violent', 0);
